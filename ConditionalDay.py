04a9ae719e17250c48783731789238e639c6d899
Jack Ashford <34728283+jashford@users.noreply.github.com>
2017-12-21 19:25:02 -0500

    Create ConditionalDay.py

---
+++ ConditionalDay.py
@@ -0,0 +1,13 @@
+def day():
+    print "You've just started your day..."
+    print "Do you go to school or do you stay home?"
+    answer = raw_input("Type school or home and hit 'Enter'.").upper()
+    if answer == "school" or answer == "0":
+        print "You have five classes to go to today and it will give you a lot of homework!"
+    elif answer == "home" or answer == "1":
+        print "You're going to be fall behind in your classes... you're not really sick"
+    else:
+        print "You didn't pick either of the options. Try again."
+        day()
+
+day()
